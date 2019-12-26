5ed5d9d1b4aa829782eeda053f6ce95434eae855
Fload <fload2000@protonmail.com>
2019-12-26 18:31:33 +0100

    Swapped from kitty to terminator

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,10 +8,10 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'kitty',
+    terminal = 'terminator',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'kitty',
+    quake = 'terminator',
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
     region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
